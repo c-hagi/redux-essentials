9f0118870a01b8bd1171068a15ffdc0678a13e1e
c-hagi <chiaki.hagiwara@sharing-innovations.co.jp>
2021-01-14 16:22:17 +0900

    update: 反応後ボタン

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,27 +1,23 @@
 import { createSlice } from '@reduxjs/toolkit'
+import { sub } from 'date-fns'
 
 const initialState = [
-  { id: '1', title: 'First Post!', content: 'Hello!' },
-  { id: '2', title: 'Second Post', content: 'More text' }
+  { content: 'Hello!', date: sub(new Date(), { minutes: 10 }).toISOString()},
+
+  { content: 'More text', date: sub(new Date(), { minutes: 5 }).toISOString()}
 ]
 
 const postsSlice = createSlice({
   name: 'posts',
   initialState,
   reducers: {
-    postAdded(state, action) {
-      state.push(action.payload)
-  },
-  postUpdated(state, action) {
-    const { id, title, content } = action.payload
-    const existingPost = state.find(post => post.id === id)
-    if (existingPost) {
-      existingPost.title = title
-      existingPost.content = content
-    }
-  }
-}
-})
-export const { postAdded } = postsSlice.actions
-
+    reactionAdded(state, action) {
+      const { postId, reaction } = action.payload
+      const existingPost = state.find(post => post.id === postId)
+      if (existingPost) {
+        existingPost.reactions[reaction]++
+      }
+    }  }
+ })
+ export const { postAdded, postUpdated, reactionAdded } = postsSlice.actions
 export default postsSlice.reducer
